5f7c9f5fa5fa8a953d6aa39ae3eae3683c9e2272
Oremanz <141567006+Oremanz@users.noreply.github.com>
2023-09-24 17:48:05 +0300

    Тут було змінено що рандомно вибирається гра

---
+++ main.py
@@ -0,0 +1,29 @@
+import sqlite3
+import random
+
+conn = sqlite3.connect("L2.db")
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM games")
+games = cursor.fetchall()
+
+def get_random_game(games):
+    if not games:
+        return "У базі даних немає ігор."
+    
+    random_game = random.choice(games)
+    game_name = random_game[1]
+    genre = random_game[2]
+    year = random_game[3]
+    studio = random_game[4]
+    
+    recommendation = f"Спробуйте гру '{game_name}' в жанрі '{genre}', випущено у {year} році від студії '{studio}'." 
+    return recommendation
+
+
+recommendation = get_random_game(games)
+print(recommendation)
+
+
+conn.commit()
